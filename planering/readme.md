640db9c9daaa949d8bea6fbde5267c1e51d78e03
Millelang <millelang2005@gmail.com>
2024-02-08 11:19:13 +0100

    landing page

--- planering/readme.md
+++ planering/readme.md
@@ -2,3 +2,7 @@
 Ska göra en databas för dortnite stats som innehåller en relation mellan spelare och deras stats. Detta ska göras med hjälp av en databas som är skapad i MySQL. 
 
 ![(/fortnitestats.drawio.png)]
+
+Gjorde en databas som innehåller en relation mellan spelare och deras stats. Detta gjordes med hjälp av en databas som är skapad i MySQL.
+
+Gjorde landing pagen för projektet.
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,21 @@ router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome' })
 })
 
+router.get('/search', function (req, res) {
+  console.log(req.query.q)
+  
+  /*
+  SELECT * FROM milton_players
+  JOIN milton_platers ON milton_stats.player_id = milton_player.id
+  WHERE milton_players.name LIKE "%QUERY%";
+  */
+
+ // let query = req.query.q
+//  query = query.replace(/[^a-zA-Z0-9]/g, '')
+
+  //res.render('search.njk', { title: 'Search', query: query })
+  res.render('search.njk', { title: 'Search' })
+})
+
+
 module.exports = router
