7408dac28f3c4e19565686a61b36141b98ae82d9
Millelang <millelang2005@gmail.com>
2024-02-12 13:20:52 +0100

    fixa replace

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,36 @@
+
 const express = require('express')
 const router = express.Router()
+
+const pool = require('../db.js')
 
 router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome' })
 })
 
+
 router.get('/search', function (req, res) {
   console.log(req.query.q)
-  
-  /*
-  SELECT * FROM milton_players
-  JOIN milton_platers ON milton_stats.player_id = milton_player.id
-  WHERE milton_players.name LIKE "%QUERY%";
-  */
 
- // let query = req.query.q
-//  query = query.replace(/[^a-zA-Z0-9]/g, '')
+  let query = req.query.q
+  query = query.replace(/[^a-zA-Z0-9]/g, '')
 
-  //res.render('search.njk', { title: 'Search', query: query })
-  res.render('search.njk', { title: 'Search' })
-})
 
+  try {
+    const id = req.params.id
+    const [player] = await.pool.promise().query('SELECT * FROM milton_player JOIN milton_stats ON milton_stats.player_id = milton_player.id WHERE milton_player.name LIKE "%query%";')
+    res.render('stats.njk', {
+      name: player.name
+    })
+
+  }
+  catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+
+
+
+});
 
 module.exports = router
